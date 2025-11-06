33c5549861abd79b9655e410631ac6d8cf89e590
MatheusNetto1 <netto.matheus123@outlook.com>
2025-11-06 19:28:04 -0300

    feat: adds the game launcher

--- engine/src/main/java/game/App.java
+++ engine/src/main/java/game/App.java
@@ -1,7 +1,10 @@
 package game;
 
+import game.core.Game;
+
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        Game game = new Game("ProtoEngine - Demo", 800, 600);
+        game.start();
     }
 }
