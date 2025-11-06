78301c6dad23d8e0c1127781a632aca0710cf1dd
MatheusNetto1 <netto.matheus123@outlook.com>
2025-11-06 19:20:35 -0300

    feat: add initial config

--- engine/src/main/java/game/config/Config.java
+++ engine/src/main/java/game/config/Config.java
@@ -1,5 +1,10 @@
 package game.config;
 
-public class Config {
-    
+public final class Config {
+    private Config() {}
+
+    public static final int TARGET_FPS = 60;
+    public static final double TIME_STEP = 1.0 / TARGET_FPS; // 60 updates per second
+    public static final double PLAYER_SPEED = 200.0;   // pixels per second
+    public static final double GRAVITY = 9.8;
 }
